f345ee49ae1694b1a4e7befcc117b31233dd695d
yota0556 <lakeille@icloud.com>
2024-09-28 11:51:17 -0400

    testing

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template
+from flask_login import login_user, logout_user, current_user, login_required
 
 app = Flask(__name__, static_url_path="/static")
 
+@app.context_processor
+def inject_user():
+    return dict(current_user=None)
 
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=False, use_reloader=False)
